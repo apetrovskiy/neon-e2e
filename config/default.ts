4faba1fd477ab99c41a9178376db40ef72cb2e19
Alexander Petrovskiy <apetrovskiy2@hotmail.com>
2021-09-22 10:31:26 +0300

    the Token object

--- config/default.ts
+++ config/default.ts
@@ -10,5 +10,9 @@ export const Config = {
   currencySymbol: getStringValue(process.env.CURRENCY_SYMBOL),
   network: getStringValue(process.env.NETWORK_NAME),
   networkId: getStringValue(process.env.NETWORK_ID),
+  tokenAddressA: getStringValue(process.env.TOKEN_A_ADDRESS),
+  tokenAddressB: getStringValue(process.env.TOKEN_B_ADDRESS),
+  tokenNameA: getStringValue(process.env.TOKEN_A_NAME),
+  tokenNameB: getStringValue(process.env.TOKEN_B_NAME),
   url: getStringValue(process.env.HTTP_URL)
 };
--- tests/step-definitions/neonswap-probe-steps.ts
+++ tests/step-definitions/neonswap-probe-steps.ts
@@ -1,6 +1,13 @@
 import { Given } from 'cucumber';
-import { ChainId } from '@uniswap/sdk';
+import { ChainId, Token } from '@uniswap/sdk';
+import { Config } from 'config/default';
 
 Given('probe', async () => {
   console.log(ChainId);
+  const chainId = parseInt(Config.networkId);
+  const tokenAddress = Config.tokenAddressA;
+  const decimals = 18;
+
+  const DAI = new Token(chainId, tokenAddress, decimals);
+  console.log(DAI);
 });
