75221da842995a51e574bad173c6aec8f4666812
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-09 06:54:59 +0300

    modified:   src/main/java/org/neonlabs/e2e/go-eth/connection.go

--- src/main/java/org/neonlabs/e2e/go-eth/connection.go
+++ src/main/java/org/neonlabs/e2e/go-eth/connection.go
@@ -1,8 +1,15 @@
 package go_eth
 
-import "github.com/ethereum/go-ethereum/ethclient"
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
 
 func connect() (*ethclient.Client, error) {
 	client, err := ethclient.Dial(GetConfig().ProxyURL)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return client, err
 }
