c27fb445f60d7b0e633e97b36610a1b7a1ddd279
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-21 21:39:41 +0300

    support for nightwatch-allure

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,5 @@
 import { NightwatchBrowser } from 'nightwatch';
+import { NightwatchAllureReporter } from 'nightwatch-allure';
 
 module.exports = {
   afterEach: (_browser: never, done: () => void) => {
@@ -6,5 +7,9 @@ module.exports = {
   },
   beforeEach: (browser: NightwatchBrowser, done: () => void) => {
     done();
+  },
+  reporter: (results: never, done: () => void) => {
+    const reporter = new NightwatchAllureReporter({ folder: './report/allure-results', sendData: true });
+    reporter.write(results, done);
   }
 };
