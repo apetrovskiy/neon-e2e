e630551766a02030297802b5a3ae3c8b6952106b
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-31 00:22:18 +0300

    modified:   src/tests/light-js/light-js.test.js

--- src/tests/light-js/light-js.test.js
+++ src/tests/light-js/light-js.test.js
@@ -1,10 +1,17 @@
 // import the actual Api class
-const { Api } = require('@parity/api');
-
+const Api = require('@parity/api');
+const makeContract = require('@parity/light.js');
 // do the setup
 describe('Light.js', () => {
   it('Connection test', () => {
     const provider = new Api.Provider.Http(process.env.PROXY_URL);
     const api = new Api(provider);
+  });
+
+  it('Contract', () => {
+    // TODO: use a real ABI
+    const ABI = '';
+    makeContract(ABI, '0x00..ff').balanceOf$('0xabc');
+    // .subscribe(console.log); // Logs the result when balance changes
   });
 });
