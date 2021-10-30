484a356deb82f3a1943586cd61bf4d86c0b4d80e
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-30 18:49:32 +0300

    modified:   src/helpers/balance.ts 	modified:   src/helpers/tx-helper.ts 	modified:   tests/step-definitions/transaction-steps.ts

--- src/helpers/balance.ts
+++ src/helpers/balance.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-import { unit } from 'src/constants/unit';
 import { Account } from 'web3-core';
 
 import { Web3Helper } from './web3-helper';
@@ -11,6 +10,6 @@ export class Balance {
     return await Web3Helper.getWeb3().eth.getBalance(account.address);
   }
   convertToEther(balance: string): string {
-    return Web3Helper.getWeb3().utils.fromWei(balance, unit.ether);
+    return Web3Helper.getWeb3().utils.fromWei(balance);
   }
 }
