565f0cb8aec0209fbbe3893d44fd8c703160e63f
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-11-05 22:06:09 +0300

    modified:   src/main/java/org/neonlabs/e2e/go-eth/config.go 	modified:   src/main/java/org/neonlabs/e2e/go-eth/wallet.go

--- src/main/java/org/neonlabs/e2e/go-eth/wallet.go
+++ src/main/java/org/neonlabs/e2e/go-eth/wallet.go
@@ -3,25 +3,34 @@ package go_eth
 import (
 	"crypto/ecdsa"
 	"fmt"
+	"log"
+
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
-	"log"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
 )
 
-func createAccount() {
+func createAccount() string {
 	privateKey, err := crypto.GenerateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 	privateKeyBytes := crypto.FromECDSA(privateKey)
-	hexString := hexutil.Encode(privateKeyBytes)
+	hexString := hexutil.Encode(privateKeyBytes)[:2]
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
 		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
 	}
-	// TODO: fix it
-	// publicKeyBytes := crypto.FromECDSA(publicKeyECDSA)
-  fmt.Println(hexString)
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(publicKeyBytes[1:])
+	fmt.Println(hexString)
 	fmt.Println(publicKeyECDSA)
+	fmt.Println(publicKeyBytes)
+	fmt.Println(address)
+	fmt.Println(hash)
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:]))
+	return hash
 }
