58e0f7d4136c93f8cc1effc2eb51ffffbf683920
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-11-06 23:04:55 +0300

    modified:   src/tests/neon-swap/first.test.ts

--- src/tests/neon-swap/first.test.ts
+++ src/tests/neon-swap/first.test.ts
@@ -5,7 +5,11 @@ import { NeonSwapMessages } from '../../constants/neon-swap';
 // import { URL } from '../constants/neon-swap ';
 
 const home: NightwatchTests = {
-  'Neon Swap': (browser: NightwatchBrowser) => {
+  'Shows the Connect wallet button': (browser: NightwatchBrowser) => {
+    browser.url(NeonSwapMessages.URL).assert.visible('#connect-wallet').end();
+    // #connect-wallet
+  },
+  'Neon Swap 2222 temp !!! 2222': (browser: NightwatchBrowser) => {
     browser.url(NeonSwapMessages.URL).assert.visible('#connect-wallet').end();
     // #connect-wallet
   }
