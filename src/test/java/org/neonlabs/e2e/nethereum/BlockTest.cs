a43a106742d5cbb25fd99f15ef07f23979a96610
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-11-06 23:01:42 +0300

    allure epic

--- src/test/java/org/neonlabs/e2e/nethereum/BlockTest.cs
+++ src/test/java/org/neonlabs/e2e/nethereum/BlockTest.cs
@@ -7,6 +7,8 @@ namespace NeonEndToEnd.Tests.org.neonlabs.e2e.nethereum
   using Xunit;
 
   [AllureSuite("Nethereum")]
+  [AllureEpic("Nethereum")]
+  [AllureFeature(new string[] { "Nethereum" })]
   public class BlockNumberTest
   {
     [AllureXunit(DisplayName = "Latest block number test")]
