8b549f2573a6cd87bb736c254aa519562c8a687e
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-02 21:34:30 +0300

    fixes

--- src/main/java/org/neonlabs/e2e/go-eth/transfer_ether.go
+++ src/main/java/org/neonlabs/e2e/go-eth/transfer_ether.go
@@ -23,27 +23,29 @@ func transferEther(client *ethclient.Client, senderAccount Account, recipientAcc
 		log.Fatal(err)
 	}
 
-	value := big.NewInt(1000000000000000000) // in wei (1 eth)
-	gasLimit := uint64(21000)                // in units
-	// gasPrice, err := client.SuggestGasPrice(context.Background())
-	// if err != nil {
-	// 	fmt.Println("=================================================================== 02 e")
-	//   fmt.Println(err)
-	// 	log.Fatal(err)
-	// }
-	gasPrice := big.NewInt(21000)
+	// value := big.NewInt(1000000000000000000) // in wei (1 eth)
+	value := new(big.Int)
+	value, _ = value.SetString(amount, 0)
+	gasLimit := uint64(21000) // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		fmt.Println("=================================================================== 02 e")
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	// gasPrice := big.NewInt(21000)
 
 	toAddress := common.HexToAddress(recipientAccount.Address.Hex())
 	var data []byte
 	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
 
-	// chainID, err := client.NetworkID(context.Background())
-	// if err != nil {
-	// 	fmt.Println("=================================================================== 03 e")
-	//   fmt.Println(err)
-	// 	log.Fatal(err)
-	// }
-	chainID := big.NewInt(111)
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		fmt.Println("=================================================================== 03 e")
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	// chainID := big.NewInt(111)
 
 	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), senderAccount.PrivateKey)
 	if err != nil {
--- src/main/java/org/neonlabs/e2e/go-eth/transfer_ether_test.go
+++ src/main/java/org/neonlabs/e2e/go-eth/transfer_ether_test.go
@@ -2,6 +2,7 @@ package go_eth
 
 import (
 	"fmt"
+	"math/big"
 	// "math/big"
 	"testing"
 
@@ -41,15 +42,17 @@ func TestTransferEther(t *testing.T) {
 			fmt.Println(recipientBalance)
 			assert.Equal(t, GetConfig().InitialBalance, recipientBalance, "Recipient's initial balance is wrong")
 
-			transferEther(client, *senderAccount, *recipientAccount, "1000000000000000000")
+			transferEther(client, *senderAccount, *recipientAccount, "10000000000000000000")
 
 			// TODO: change to the right amounts
 			senderBalance = getLastBlockBalance(client, senderAccount.Address.Hex())
 			fmt.Println(senderBalance)
-			assert.Equal(t, GetConfig().InitialBalance, senderBalance, "Sender's initial balance is wrong")
+			expectedSenderBalance, _ := new(big.Int).SetString("90000000000000000000", 0)
+			assert.Equal(t, expectedSenderBalance, senderBalance, "Sender's initial balance is wrong")
 
 			recipientBalance = getLastBlockBalance(client, recipientAccount.Address.Hex())
 			fmt.Println(recipientBalance)
-			assert.Equal(t, GetConfig().InitialBalance, recipientBalance, "Recipient's initial balance is wrong")
+			expectedRecipientBalance, _ := new(big.Int).SetString("110000000000000000000", 0)
+			assert.Equal(t, expectedRecipientBalance, recipientBalance, "Recipient's initial balance is wrong")
 		}))
 }
