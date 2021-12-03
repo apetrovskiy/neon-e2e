afcc4be6d66df184e6eca74704d7b638b8b20723
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-03 15:20:40 +0300

    experimental changes

--- src/main/java/org/neonlabs/e2e/go-eth/wallet_test.go
+++ src/main/java/org/neonlabs/e2e/go-eth/wallet_test.go
@@ -36,3 +36,34 @@ func TestConnection111(t *testing.T) {
 			}
 		}))
 }
+
+func TestConnection333(t *testing.T) {
+
+	allure.Test(t,
+		allure.Epic("go-ethereum"),
+		allure.Feature("go-ethereum"),
+		allure.Story("Connection 333"),
+		allure.Description("Connection to network"),
+		allure.Action(func() {
+			_, err := connect()
+			if err != nil {
+				t.Errorf("Failed to connect to %s: %o", GetConfig().ProxyURL, err)
+			}
+		}))
+}
+
+func TestNewWalletBalance333(t *testing.T) {
+
+	allure.Test(t,
+		allure.Epic("go-ethereum"),
+		allure.Feature("go-ethereum"),
+		allure.Story("Wallet 333"),
+		allure.Description("Creating a new wallet 222"),
+		allure.Action(func() {
+			account := createWallet()
+			if len(account.Address.Hash()) == 0 {
+				t.Error("Failed to create a new wallet")
+			}
+			t.Log("Wallet has been created")
+		}))
+}
