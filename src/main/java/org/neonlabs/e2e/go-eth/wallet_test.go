ae94ba3b080622a15435d2510224a4d222477bad
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-03 15:38:07 +0300

    Update wallet_test.go

--- src/main/java/org/neonlabs/e2e/go-eth/wallet_test.go
+++ src/main/java/org/neonlabs/e2e/go-eth/wallet_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/dailymotion/allure-go"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewWalletBalance(t *testing.T) {
@@ -16,9 +17,11 @@ func TestNewWalletBalance(t *testing.T) {
 		allure.Action(func() {
 			account := createWallet()
 			if len(account.Address.Hash()) == 0 {
+				assert.NotEqual(t, 0, len(account.Address.Hash()), "Failed to create a new wallet")
 				t.Error("Failed to create a new wallet")
 			}
 			t.Log("Wallet has been created")
+
 		}))
 }
 
@@ -43,7 +46,7 @@ func TestConnection333(t *testing.T) {
 		allure.Epic("go-ethereum"),
 		allure.Feature("go-ethereum"),
 		allure.Story("Connection 333"),
-		allure.Description("Connection to network"),
+		allure.Description("Connection to network 333"),
 		allure.Action(func() {
 			_, err := connect()
 			if err != nil {
@@ -58,7 +61,7 @@ func TestNewWalletBalance333(t *testing.T) {
 		allure.Epic("go-ethereum"),
 		allure.Feature("go-ethereum"),
 		allure.Story("Wallet 333"),
-		allure.Description("Creating a new wallet 222"),
+		allure.Description("Creating a new wallet 333"),
 		allure.Action(func() {
 			account := createWallet()
 			if len(account.Address.Hash()) == 0 {
