d45e09bbfaf046101fe30d1e3a3365321b94562a
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-03 20:36:04 +0300

    Feature/149 increase the number of nethereum tests (#75)
    * allure epic
    
    * Update ConnectionTest.cs
    
    * Update Config.cs
    
    * fix of Config.cs
    
    * refactoring; .NET 6.0

---
+++ src/test/java/org/neonlabs/e2e/nethereum/Constants.cs
@@ -0,0 +1,21 @@
+namespace NeonEndToEnd.org.neonlabs.e2e.nethereum
+{
+  public static class Constants
+  {
+    public const string Suite = "Nethereum";
+    public const string Epic = "Nethereum";
+    public const string FeatureAddress = "Address";
+    public const string FeatureBlocks = "Blocks";
+    public const string FeatureCommon = "Common";
+    public const string FeatureEvents = "Events";
+    public const string FeatureExternallyOwnedAccounts = "Externally Owned Accounts";
+    public const string FeatureSmartContracts = "Smart contracts";
+    public const string FeatureSwarm = "Swarm";
+    public const string FeatureWhisper = "Whisper";
+    public const string StoryAddressValidation = "Address validation";
+    public const string StoryBalance = "Balance";
+    public const string StoryConnection = "Connection";
+    public const string StoryTransfer = "Transfer";
+    public const string StoryWallet = "Wallet";
+  }
+}
