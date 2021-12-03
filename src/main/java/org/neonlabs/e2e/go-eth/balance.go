fed1156d164bda10944776a4591130389304b27c
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-03 21:52:59 +0300

    Update balance.go

--- src/main/java/org/neonlabs/e2e/go-eth/balance.go
+++ src/main/java/org/neonlabs/e2e/go-eth/balance.go
@@ -2,35 +2,52 @@ package go_eth
 
 import (
 	"context"
+	"github.com/dailymotion/allure-go"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-
 	"log"
-
 	"math/big"
 )
 
 func getLastBlockBalance(client *ethclient.Client, hex string) *big.Int {
-	account := common.HexToAddress(hex)
-	balance, err := client.BalanceAt(context.Background(), account, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var balance *big.Int
+	var err error
+
+	allure.Step(allure.Description("Getting last block balance"), allure.Action(func() {
+
+		account := common.HexToAddress(hex)
+		balance, err = client.BalanceAt(context.Background(), account, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}))
 	return balance
 }
 func getSpecificBlockBalance(client *ethclient.Client, hex string, blockNumber *big.Int) *big.Int {
-	account := common.HexToAddress(hex)
-	balance, err := client.BalanceAt(context.Background(), account, blockNumber)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var balance *big.Int
+	var err error
+
+	allure.Step(allure.Description("Getting a specific block balance"), allure.Action(func() {
+
+		account := common.HexToAddress(hex)
+		balance, err = client.BalanceAt(context.Background(), account, blockNumber)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}))
 	return balance
 }
 func getPendingBalance(client *ethclient.Client, hex string) *big.Int {
-	account := common.HexToAddress(hex)
-	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var pendingBalance *big.Int
+	var err error
+
+	allure.Step(allure.Description("Getting penging balance"), allure.Action(func() {
+
+		account := common.HexToAddress(hex)
+		pendingBalance, err = client.PendingBalanceAt(context.Background(), account)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}))
 	return pendingBalance
 }
