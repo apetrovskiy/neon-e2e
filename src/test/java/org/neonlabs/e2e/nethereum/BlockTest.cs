97a4984e9aaca718a8a3c1a18ea1951be43e6780
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-09 19:32:53 +0300

    Feature/149 increase the number of nethereum tests (#80)
    * allure feature
    
    * Update frog_jmp_test.go
    
    * allure epic
    
    * Update ConnectionTest.cs
    
    * the get latest block's balance test
    
    * a specific block's balance; pending balance
    
    * Update balance_test.go
    
    * experimental changes
    
    * refactoring; preparation to transfer tests
    
    * fixes
    
    * fixes
    
    * updates of .env files
    
    * updates
    
    * refactoring
    
    * intermittent changes
    
    * experimental changes (with hard-code)
    
    * Update transfer_ether.go
    
    * experimental changes
    
    * refactoring of a config property; fixes in Java tests
    
    * fixes; temporary changes
    
    * fixes
    
    * allure steps
    
    * more allure steps
    
    * refactoring
    
    * Update transfer_helper.go
    
    * refactoring; more allure steps
    
    * refactoring; experiments
    
    * .
    
    * Update wallet_test.go
    
    * refactoring in allure names
    
    * experimental changes
    
    * experimental changes
    
    * experimental changes
    
    * Update wallet_test.go
    
    * refactoring
    
    * refactoring
    
    * test preparation
    
    * refactoring
    
    * more test cases planned
    
    * code clean-up
    
    * Update Config.cs
    
    * fix of Config.cs
    
    * refactoring; .NET 6.0
    
    * refactoring
    
    * Update balance.go
    
    * logrus instead of the standard log
    
    * constants
    
    * test preparation; refactoring
    
    * modified:   src/test/java/org/neonlabs/e2e/nethereum/Constants.cs
    
    * modified:   src/main/java/org/neonlabs/e2e/go-eth/transfer_token_test.go
    	new file:   src/test/java/org/neonlabs/e2e/nethereum/BalanceTest.cs
    	modified:   src/test/java/org/neonlabs/e2e/nethereum/BlockTest.cs
    	modified:   src/test/java/org/neonlabs/e2e/nethereum/ConnectionTest.cs
    	new file:   src/test/java/org/neonlabs/e2e/nethereum/EventsTest.cs
    	new file:   src/test/java/org/neonlabs/e2e/nethereum/SmartContractsTest.cs
    	new file:   src/test/java/org/neonlabs/e2e/nethereum/TransactionTest.cs
    	new file:   src/test/java/org/neonlabs/e2e/nethereum/TranserEtherTest.cs
    	new file:   src/test/java/org/neonlabs/e2e/nethereum/TransferTokenTest.cs
    	new file:   src/test/java/org/neonlabs/e2e/nethereum/WalletTest.cs

--- src/test/java/org/neonlabs/e2e/nethereum/BlockTest.cs
+++ src/test/java/org/neonlabs/e2e/nethereum/BlockTest.cs
@@ -12,8 +12,28 @@ namespace NeonEndToEnd.Tests.org.neonlabs.e2e.nethereum
   [AllureFeature(new string[] { FeatureBlocks })]
   public class BlockNumberTest
   {
+    [AllureSubSuite(StoryQueryBlock)]
+    [AllureStory(StoryQueryBlock)]
     [AllureXunit(DisplayName = "Latest block number test")]
     public async void ShouldGetLatestBlockNumber()
+    {
+      var latestBlockNumber = await Block.GetLatestBlockNumber();
+      Assert.NotNull(latestBlockNumber);
+    }
+    [AllureSubSuite(StoryQueryBlock)]
+    [AllureStory(StoryQueryBlock)]
+    [AllureXunit(DisplayName = "Block header")]
+    [Fact(Skip = "not yet done")]
+    public async void ShouldGetBlockHeader()
+    {
+      var latestBlockNumber = await Block.GetLatestBlockNumber();
+      Assert.NotNull(latestBlockNumber);
+    }
+    [AllureSubSuite(StoryQueryBlock)]
+    [AllureStory(StoryQueryBlock)]
+    [AllureXunit(DisplayName = "Full block")]
+    [Fact(Skip = "not yet done")]
+    public async void ShouldGetFullBlock()
     {
       var latestBlockNumber = await Block.GetLatestBlockNumber();
       Assert.NotNull(latestBlockNumber);
